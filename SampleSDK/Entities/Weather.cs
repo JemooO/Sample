3b03d7ff875c7ab5f1ff110e881dde8e68d74254
JemooO <jemo800@gmail.com>
2020-02-10 17:59:10 +0200

    initial git

---
+++ SampleSDK/Entities/Weather.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SampleSDK.Entities
+{
+    public class Weather
+    {
+        public DateTime date { get; set; }
+
+        public float tempratureC { get; set; }
+
+        public float tempratureF { get; set; }
+
+        public string summery { get; set; }
+    }
+}
